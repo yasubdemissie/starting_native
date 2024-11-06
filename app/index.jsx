2c8a445837f4af1e0001d27adb4580511c72b095
yasubdemissie <yasdam777@gmail.com>
2024-11-06 17:41:30 +0300

    first native app commit

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#303030",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>My name is yasub demissie.</Text>
+    </View>
+  );
+}
