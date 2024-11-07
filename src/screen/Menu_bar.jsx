947f9c07610f471e85832dbf4a3d1fb007e2896b
yasubdemissie <yasdam777@gmail.com>
2024-11-07 15:47:08 +0300

    2 commit

---
+++ src/screen/Menu_bar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Menu_bar = () => {
+  return (
+    <View>
+      <Text>Menu Bar</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Menu_bar;
